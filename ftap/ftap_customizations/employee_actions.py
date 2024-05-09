9853e672024f8734839b230d3228666bf8085df6
tonancheta <trancheta@gmail.com>
2024-05-10 00:36:21 +0800

    Added the Mobile Summary tab in Employee form

--- ftap/ftap_customizations/employee_actions.py
+++ ftap/ftap_customizations/employee_actions.py
@@ -14,3 +14,37 @@ def create_ftap_info(employee):
         msg = _("Employee FTAP Info already exists for this employee")
 
     return msg
+
+
+@frappe.whitelist()
+def get_ftap_info(employee, source_fields):
+    print('** get_ftap_info **\n' + employee + '\n')
+
+    if frappe.db.exists('Employee FTAP Information', employee):
+        # Fetch Employee FTAP Info document
+        doc = frappe.get_doc('Employee FTAP Information', employee)
+
+        # Retrieve each element in source_fields list, fetch the field value from the document and store it in a dictionary
+        source_fields = frappe.parse_json(source_fields)
+        field_values = {field: doc.get(field) for field in source_fields}
+
+        return field_values
+    else:
+        return None
+    
+
+
+@frappe.whitelist()
+def update_ftap_info(employee, field_values):
+    if frappe.db.exists('Employee FTAP Information', employee):
+        # Fetch Employee FTAP Info document
+        doc = frappe.get_doc('Employee FTAP Information', employee)
+
+        # Update the value of each field in field_values dictionary
+        for field, value in field_values.items():
+            doc.set(field, value)
+
+        doc.save()
+        return _("Employee FTAP Info updated successfully")
+    else:
+        return _("Employee FTAP Info does not exist for this employee")
--- ftap/public/js/employee.js
+++ ftap/public/js/employee.js
@@ -4,8 +4,58 @@ frappe.ui.form.on('Employee', {
             frappe.set_route('Form', 'Employee FTAP Information', frm.doc.name);
         })
     },
+    
+    // onload: function(frm) {
+    //     source_fields = [
+    //         'mobile_last_name', 'mobile_middle_name', 'mobile_first_name', 'mobile_date_hired', 'mobile_department',
+    //         'mobile_immediate_supervisor', 'manager_', 'ftap_department_head', 'region', 'mobile_branch',
+    //         'ftap_tin_number', 'ftap_sss_number', 'ftap_philhealth_number', 'ftap_pagibig_number',
+    //         'mobile_date_of_birth', 'mobile_gender', 'mobile_nationality', 'mobile_marital_status', 'mobile_contact_no',
+    //         'mobile_company_email', 'mobile_personal_email', 'mobile_perm_add', 'mobile_present_add', 'mobile_home_add',
+    //         'emergency_contact_no'
+    //     ];
 
-    // Automatically create a record in Employee FTAP Information when a new Employee record is created
+    //     frappe.call({
+    //         method: 'ftap.ftap_customizations.employee_actions.get_ftap_info',
+    //         args: {
+    //             'employee': frm.doc.name,
+    //             'source_fields': source_fields,
+    //         },
+    //         callback: function(r) {
+    //             if (r.message) {
+    //                 // Get the first element of the returned list into mobile_employee_code
+    //                 frm.set_value('custom_mobile_employee_code', frm.doc.name);
+    //                 frm.set_value('custom_mobile_last_name', r.message['mobile_last_name']);
+    //                 frm.set_value('custom_mobile_middle_name', r.message['mobile_middle_name']);
+    //                 frm.set_value('custom_mobile_first_name', r.message['mobile_first_name']);
+    //                 frm.set_value('custom_mobile_date_hired', r.message['mobile_date_hired']);
+    //                 frm.set_value('custom_mobile_department', r.message['mobile_department']);
+    //                 frm.set_value('custom_mobile_immediate_supervisor', r.message['mobile_immediate_supervisor']);
+    //                 frm.set_value('custom_mobile_manager_1', r.message['manager_']);
+    //                 frm.set_value('custom_mobile_department_head', r.message['ftap_department_head']);
+    //                 frm.set_value('custom_mobile_region', r.message['region']);
+    //                 frm.set_value('custom_mobile_branch', r.message['mobile_branch']);
+    //                 frm.set_value('custom_mobile_tin', r.message['ftap_tin_number']);
+    //                 frm.set_value('custom_mobile_sss', r.message['ftap_sss_number']);
+    //                 frm.set_value('custom_mobile_philhealth', r.message['ftap_philhealth_number']);
+    //                 frm.set_value('custom_mobile_pagibig', r.message['ftap_pagibig_number']);
+    //                 frm.set_value('custom_mobile_date_of_birth', r.message['mobile_date_of_birth']);
+    //                 frm.set_value('custom_mobile_gender', r.message['mobile_gender']);
+    //                 frm.set_value('custom_mobile_nationality', r.message['mobile_nationality']);
+    //                 frm.set_value('custom_mobile_marital_status', r.message['mobile_marital_status']);
+    //                 frm.set_value('custom_mobile_contact_no', r.message['mobile_contact_no']);
+    //                 frm.set_value('custom_mobile_company_email', r.message['mobile_company_email']);
+    //                 frm.set_value('custom_mobile_personal_email', r.message['mobile_personal_email']);
+    //                 frm.set_value('custom_mobile_permanent_address_local', r.message['mobile_perm_add']);
+    //                 frm.set_value('custom_mobile_present_address_local', r.message['mobile_present_add']);
+    //                 frm.set_value('custom_mobile_home_address_abroad', r.message['mobile_home_add']);
+    //                 frm.set_value('custom_mobile_emergency_contact_person', frm.doc.emergency_contact_person);
+    //                 frm.set_value('custom_mobile_emergency_contact_no', r.message['emergency_contact_no']);
+    //             }
+    //         }
+    //     });
+    // },
+
     after_save: function(frm) {
         frappe.call({
             method: 'ftap.ftap_customizations.employee_actions.create_ftap_info',
@@ -23,5 +73,5 @@ frappe.ui.form.on('Employee', {
     custom_open_ftap_info: function(frm) {
         // Open the Employee FTAP Information doctype with this Employee record already loaded
         frappe.set_route('Form', 'Employee FTAP Information', frm.doc.name);
-    }
+    },
 });
